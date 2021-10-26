9c2760d241d01d5e8915a122399f14d0fcbfc281
Arthur Pierrot <arthur.pierrot@hotmail.com>
2021-10-26 20:47:37 +0200

    INIT

---
+++ devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(beginner_tutorials_MESSAGE_FILES "/home/jepsiko/catkin_ws/src/beginner_tutorials/msg/Num.msg")
+set(beginner_tutorials_SERVICE_FILES "/home/jepsiko/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
